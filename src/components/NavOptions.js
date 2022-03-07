67c16a26500faf7bf09fdeeddbe123e91cd5c5ec
Abdun Noor Faruki Biswas <abdunnoorfarukibiswas6761.gmain.com>
2022-03-07 20:46:37 +0530

    Today 07-03-2022 I add login functionaliti into this project using firebase google authentication

--- src/components/NavOptions.js
+++ src/components/NavOptions.js
@@ -5,7 +5,7 @@ function NavOptions({ Icon, text, className, imgSrc, onclick }) {
   return (
 
     <li className={styles.navOptions} onClick={onclick}>
-      {imgSrc ? <img src={imgSrc} /> : (Icon && <Icon />)}
+      {imgSrc ? <img src={imgSrc} alt='img' /> : (Icon && <Icon />)}
 
       <h3>{text}</h3>
     </li>
